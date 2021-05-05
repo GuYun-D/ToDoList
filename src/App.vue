7a7fb336ed5c1f5ef36322d69bbc6259799a91e0
GuYun-D <1946306647@qq.com>
2021-05-05 14:44:26 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <Header></Header>
+      <List></List>
+      <Footer></Footer>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import Footer from './components/Footer'
+import List from './components/List'
+
+export default {
+  name: "app",
+  components: {
+    Header,
+    Footer,
+    List,
+  },
+};
+</script>
+
+<style>
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+</style>
